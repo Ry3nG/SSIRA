93a0396fe81821ed159df9ef592534a81f1fd234
GongZerui <gong0060@e.ntu.edu.sg>
2023-11-22 23:55:08 +0800

    updated hyperparam and added scheduler for LR

--- code/TAD66K/train.py
+++ code/TAD66K/train.py
@@ -1,14 +1,18 @@
+import os
 import torch
 import torch.optim as optim
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 from torch.cuda.amp import GradScaler, autocast
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 import torchvision.transforms as transforms
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 from skimage.metrics import peak_signal_noise_ratio as compare_psnr
 
@@ -18,19 +22,17 @@ from models.autoencoder import ConvAutoencoder
 import logging
 import datetime
 
-today = datetime.date.today()
+current_time = datetime.datetime.now()
 
-
-# set up logging
+# set up logging console output format
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(), logging.FileHandler(f"train_{current_time}.log")],
 )
 
-
 logging.info("Imported packages.")
-logging.info("Training script started on %s." % today)
+logging.info("Training script started on %s." % current_time)
 
 # constants
 PATH_DATASET_TAD66K = "/home/zerui/SSIRA/dataset/TAD66K/"
@@ -60,14 +62,15 @@ degradation_transform = transforms.Compose(
         transforms.Resize((256, 256)),  # Match the original transform size
         transforms.RandomHorizontalFlip(),  # Randomly flip the images horizontally
         transforms.ColorJitter(
-            brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2
+            brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
         ),  # Random color jitter,
-        transforms.GaussianBlur(kernel_size=(9, 9), sigma=(0.1, 5)),
+        transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
         transforms.Lambda(
-            lambda x: x + 0.1 * torch.randn_like(x)
+            transforms.Lambda(lambda x: x + 0.05 * torch.randn_like(x))
         ),  # Adding random noise
     ]
 )
+
 
 # load the dataset
 train_dataset = TAD66KDataset(
@@ -82,14 +85,15 @@ val_size = len(train_dataset) - train_size
 train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
 
 
-train_loader = DataLoader(train_dataset, batch_size=512, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=512, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=8)
+val_loader = DataLoader(val_dataset, batch_size=256, shuffle=True, num_workers=8)
 
 logging.info(f"Train dataset size: {len(train_dataset)}")
 logging.info(f"Validation dataset size: {len(val_dataset)}")
 
 # initialize the model
 model = ConvAutoencoder()
+
 if torch.cuda.device_count() > 1:
     logging.info(f"Using {torch.cuda.device_count()} GPUs!")
     model = torch.nn.DataParallel(model)
@@ -98,21 +102,26 @@ model.to(device)
 logging.info(
     f"Number of trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
 )
+logging.info("Model initialized.")
 
 
 # define the loss function and optimizer
 criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001 * 16)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+# Set up the scheduler
+scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, verbose=True) 
+logging.info("Scheduler initialized.")
 
 # train the model
-early_stopping_patience = 3  # Number of epochs to wait for improvement before stopping
+early_stopping_patience = 5  # Number of epochs to wait for improvement before stopping
 best_val_loss = float("inf")  # Initialize best validation loss as infinity
 epochs_no_improve = 0  # Counter for epochs with no improvement
 
-num_epochs = 20
+num_epochs = 1000
 best_val_loss = float("inf")  # Initialize best validation loss as infinity
 early_stopping_patience = 3  # Number of epochs to wait for improvement before stopping
 epochs_no_improve = 0  # Counter for epochs with no improvement
+val_loss_history = []
 
 scaler = GradScaler()
 
@@ -143,14 +152,15 @@ for epoch in range(num_epochs):
 
         running_loss += loss.item()
 
-        logging.info(
-            f"Epoch [{epoch+1}/{num_epochs}], Batch [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}"
-        )
+        if i % 10 == 0:
+            logging.info(
+                f"Epoch [{epoch+1}/{num_epochs}], Batch [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}"
+            )
 
     # Validation phase
     model.eval()
     val_loss = 0.0
-    total_psnr = 0.0
+    #total_psnr = 0.0
     with torch.no_grad():
         for degraded_images, original_images in val_loader:
             degraded_images, original_images = degraded_images.to(
@@ -175,10 +185,13 @@ for epoch in range(num_epochs):
                     reconstructed.max() - reconstructed.min()
                 )
 
-                total_psnr += compare_psnr(orig, reconstructed)
+                #total_psnr += compare_psnr(orig, reconstructed)
 
     avg_val_loss = val_loss / len(val_loader)
-    avg_psnr = total_psnr / len(val_loader.dataset)
+    val_loss_history.append(avg_val_loss)
+
+    #avg_psnr = total_psnr / len(val_loader.dataset)
+    scheduler.step(avg_val_loss)
 
     # Early stopping check
     if avg_val_loss < best_val_loss:
@@ -192,11 +205,20 @@ for epoch in range(num_epochs):
         break
 
     logging.info(
-        f"Epoch {epoch+1}/{num_epochs}, Train Loss: {running_loss/len(train_loader)}, Val Loss: {avg_val_loss}, Avg PSNR: {avg_psnr}"
+        f"Epoch {epoch+1}/{num_epochs}, Train Loss: {running_loss/len(train_loader)}, Val Loss: {avg_val_loss}"
     )
+    plt.figure(figsize=(10, 5))
+    plt.plot(val_loss_history, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.title('Validation Loss Over Epochs')
+    plt.savefig(f"val_loss_{current_time}.png")
+    plt.close()
+
 
 # Save the trained model
 if isinstance(model, torch.nn.DataParallel):
-    torch.save(model.module.state_dict(), f"conv_autoencoder_{today}.pth")
+    torch.save(model.module.state_dict(), f"conv_autoencoder_{current_time}.pth")
 else:
-    torch.save(model.state_dict(), f"conv_autoencoder_{today}.pth")
+    torch.save(model.state_dict(), f"conv_autoencoder_{current_time}.pth")
