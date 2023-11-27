933cc5b5b7baebd56020d9f096dbe8e06925f1dc
GongZerui <gong0060@e.ntu.edu.sg>
2023-11-27 13:26:19 +0800

    directory modification

--- code/TAD66K/utils/constants.py
+++ code/TAD66K/utils/constants.py
@@ -11,15 +11,18 @@ PATH_LABEL_MERGE_TAD66K_TRAIN = (
 PATH_LABEL_DEGRADATION_PRETEXT_TAD66K_TRAIN = "/home/zerui/SSIRA/dataset/TAD66K/labels/degradation_pretext/train.csv"
 PATH_LABEL_DEGRADATION_PRETEXT_TAD66K_TEST = "/home/zerui/SSIRA/dataset/TAD66K/labels/degradation_pretext/test.csv"
 
+PATH_AVA_TXT = "/home/zerui/SSIRA/dataset/AVA/AVA_dataset/AVA.txt"
+PATH_AVA_IMAGE = "/home/zerui/SSIRA/dataset/AVA/AVA_dataset/image"
+
 #result constants
 PATH_LOGS = "/home/zerui/SSIRA/code/TAD66K/results/logs/"
 PATH_MODELS = "/home/zerui/SSIRA/code/TAD66K/results/models/"
 PATH_PLOTS = "/home/zerui/SSIRA/code/TAD66K/results/plots/"
 
 # training constants
-BATCH_SIZE = 512
+BATCH_SIZE = 64
 NUM_EPOCHS = 1000
-LEARNING_RATE = 0.001
+LEARNING_RATE = 1e-5
 NUM_WORKERS = 8
 TRAIN_VAL_SPLIT_RATIO = 0.9
 
