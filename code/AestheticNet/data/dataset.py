7fd50d37b1909d9d2dfb90d2e9301706216400fe
GongZerui <gong0060@e.ntu.edu.sg>
2023-11-29 11:04:34 +0800

    fixed dataset filtering logic based on ID (AVA)

--- code/AestheticNet/data/dataset.py
+++ code/AestheticNet/data/dataset.py
@@ -43,26 +43,26 @@ class TAD66KDataset(Dataset):
         return len(self.data_frame)
 
     def __getitem__(self, idx):
-        try:
-            img_name = os.path.join(self.root_dir, self.data_frame.iloc[idx, 0])
-            image = Image.open(img_name).convert("RGB")
-        except (OSError, UnidentifiedImageError) as e:
-            logging.error(f"Error opening image: {img_name}")
-            # handle the error by skipping the image
-            return self.__getitem__(idx + 1)
-        
+        max_attempts = 10  # for instance
+        for _ in range(max_attempts):
+            try:
+                img_name = os.path.join(self.root_dir, self.data_frame.iloc[idx % len(self.data_frame), 0])
+                image = Image.open(img_name).convert("RGB")
+                if self.transform:
+                    image = self.transform(image)
+                return image
+            except (OSError, UnidentifiedImageError) as e:
+                logging.error(f"Error opening image: {img_name}, attempting next image.")
+                idx += 1
+        logging.error(f"Could not find a valid image after {max_attempts} attempts.")
 
-        if self.transform:
-            image = self.transform(image)
-
-        return image
 
 
 
 class AVADataset(Dataset):
     """AVA dataset for Aesthetic Score Prediction"""
 
-    def __init__(self, txt_file, root_dir, custom_transform_options, default_transform = True):
+    def __init__(self, txt_file, root_dir, custom_transform_options, default_transform = True, ids = None, include_ids = True):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -79,29 +79,51 @@ class AVADataset(Dataset):
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ]
         self.transform = transforms.Compose(transform_list)
+        self.ids = ids
+        self.include_ids = include_ids
+
+        if self.ids is not None:
+            # Ensure IDs are of the same type as in the data_frame
+            self.ids = [int(id) for id in self.ids]  # Convert to int if they are strings
+            if self.include_ids:
+                # Keep only rows with IDs in the list
+                self.data_frame = self.data_frame[self.data_frame[1].isin(self.ids)]
+            else:
+                # Exclude rows with IDs in the list
+                self.data_frame = self.data_frame[~self.data_frame[1].isin(self.ids)]
+
+        logging.info(f"Filtered dataset size: {len(self.data_frame)}")
+
 
     def __len__(self):
         return len(self.data_frame)
 
     def __getitem__(self, idx):
-        try:
-            image_id = self.data_frame.iloc[idx, 1]
-            img_name = os.path.join(self.root_dir, f"{image_id}.jpg")
-            image = Image.open(img_name).convert("RGB")
-        except (OSError, UnidentifiedImageError) as e:
-            logging.error(f"Error opening image: {img_name}")
-            # handle the error by skipping the image
-            return self.__getitem__(idx + 1)
-        # compute the aesthetic score as the average of the ratings
-        ratings = self.data_frame.iloc[idx, 2:12].values
-        score = sum((i + 1) * ratings[i] for i in range(10)) / sum(ratings)
+        max_attempts = 10  # Maximum number of attempts to find a valid image
+        attempts = 0
 
-        if self.transform:
-            image = self.transform(image)
+        while attempts < max_attempts:
+            try:
+                image_id = self.data_frame.iloc[idx, 1]
+                img_name = os.path.join(self.root_dir, f"{image_id}.jpg")
+                image = Image.open(img_name).convert("RGB")
 
-        return image, score
+                if self.transform:
+                    image = self.transform(image)
 
+                # compute the aesthetic score as the average of the ratings
+                ratings = self.data_frame.iloc[idx, 2:12].values
+                score = sum((i + 1) * ratings[i] for i in range(10)) / sum(ratings)
 
+                return image, score
+
+            except (OSError, UnidentifiedImageError) as e:
+                logging.error(f"Error opening image: {img_name}, attempting next image.")
+                idx = (idx + 1) % len(self.data_frame)  # Move to the next index
+                attempts += 1
+        
+        logging.error(f"Could not find a valid image after {max_attempts} attempts.")
+ 
 # Example instantiation of the dataset
 # tad66k_dataset = TAD66KDataset(csv_file='path_to_tad66k_csv', root_dir='path_to_tad66k_images', transform=transform)
 # ava_dataset = AVADataset(txt_file='path_to_ava_txt', root_dir='path_to_ava_images', transform=transform)
